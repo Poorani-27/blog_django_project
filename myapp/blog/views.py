9d82fa485e31e275475540d89832d5e352ce903a
POORANI TAMILVASAN <125793555+Poorani-27@users.noreply.github.com>
2024-06-19 00:06:57 +0530

    for and if tags

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,7 +5,16 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     blog_title="Today's Post"
-    return render(request,"index.html", {'blog_title':blog_title})
+    posts=[
+        {'title':"post 1",'content':"Content 0f post 1", 'area':"Sports"},
+        {'title':"post 2",'content':"Content 0f post 2", 'area':"movies"},
+        {'title':"post 3",'content':"Content 0f post 3", 'area':"Science"},
+        {'title':"post 4",'content':"Content 0f post 4", 'area':"Development"},
+        {'title':"post 5",'content':"Content 0f post 5", 'area':"culture"},
+        {'title':"post 6",'content':"Content 0f post 6", 'area':"History"}
+        
+    ]
+    return render(request,"index.html", {'blog_title':blog_title , 'posts':posts})
 
 def post(request, post_id):
     return render(request,"details.html")
