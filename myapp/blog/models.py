4bde6cbc3aa4f447206489454ceaeea327634e7d
POORANI TAMILVASAN <125793555+Poorani-27@users.noreply.github.com>
2024-06-20 06:19:53 +0530

    make migrations

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -5,6 +5,7 @@ class post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
     img_url=models.URLField(blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
     
 def __str__(self):
     return self.title
